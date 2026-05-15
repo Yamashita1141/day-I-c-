bc3620806abead9f75128877dcfd5e190e0b5c13
YAMA <yosiroyamasita@gmail.com>
2026-05-15 16:21:07 +0700

    Add files via upload

---
+++ variabel_tipedata.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //tipe data integer(bilangan bulat)
+    int umur = 21;
+    
+    //tipe data float(bilangan desimal)
+    float tinggi = 177.5;
+
+    //tipe data karakter(simbol/huruf)
+    char nilai = 'A';
+
+    //tipe data untuk true or false(benar/salah)
+    bool motor = true;
+
+    //tipe data teks
+    string nama = "Yama";
+
+    //cetak/output
+    cout << "umurku: " << umur << endl;
+    cout << "tinggiku: " << tinggi << endl;
+    cout << "di kampus aku dapat nilai " << nilai << " alhamdulillah banget" << endl;
+    cout << "dan aku sudah punya " << motor << " motor di kos" << endl;
+    cout << "oh iya namaku: " << nama << " salam kenal ya!" << endl;
+    cout << "ini day 1 ku belajar c++, mungkin telat tapi gpp doakan ya biar bisa lancar coding dan bisa lanjut belajar hal lain" << endl;
+};
